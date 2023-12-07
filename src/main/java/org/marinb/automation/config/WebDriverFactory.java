e4e826f4261b66aaccb82fb915c72508e9b865af
LuisM11 <lumarinb@unal.edu.co>
2023-12-07 00:23:06 -0500

    Logging properties and driver factory

---
+++ src/main/java/org/marinb/automation/config/WebDriverFactory.java
@@ -0,0 +1,36 @@
+package org.marinb.automation.config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+import java.io.File;
+
+
+public class WebDriverFactory {
+    public static WebDriver createDriver(String browser) {
+        switch (browser) {
+            case "chrome":
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addExtensions(new File("src/test/resources/AdBlock-el-mejor-bloqueador-de-anuncios.crx"));
+                return new ChromeDriver(chromeOptions);
+//                return  new ChromeDriver();
+            case "firefox":
+                FirefoxProfile firefoxProfile = new FirefoxProfile();
+                firefoxProfile.addExtension(new File("src/test/resources/adblockultimate@adblockultimate.net.xpi"));
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.setProfile(firefoxProfile);
+                return new FirefoxDriver(firefoxOptions);
+//                return new FirefoxDriver();
+            case "edge":
+                return new EdgeDriver();
+            default:
+                throw new IllegalArgumentException("Invalid browser name");
+        }
+    }
+
+}
